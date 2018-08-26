5d28d1b2a8b5d43d242ea7f1c08f549ffaa68027
LuckyTiger3 <nikita2501400@gmail.com>
2018-08-26 14:51:53 +0300

    Task-2-2

---
+++ src/Task_2_2.java
@@ -0,0 +1,20 @@
+
+public class Task_2_2 {
+
+	public static void main(String[] args) {
+		int x = 20;
+		int y = 10;
+		// Тут я по номерам разбил задачи.
+		int task1 = x + y;
+		int task2 = y - x;
+		int task3 = y / x;
+		int task4 = y * y;
+		int task5 = (x + y) / x;
+		System.out.println(task1);
+		System.out.println(task2);
+		System.out.println(task3);
+		System.out.println(task4);
+		System.out.println(task5);
+	}
+
+}
