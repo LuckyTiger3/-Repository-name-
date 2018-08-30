b74aab55bc2a4c6c54c9b19fb771e5bf3721d417
LuckyTiger3 <nikita2501400@gmail.com>
2018-08-30 23:04:33 +0300

    Task 3 1

---
+++ src/Task_3_1.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class Task_3_1 {
+
+	public static void main(String[] args) {
+		
+		
+		int[] x = {213,3213,321,2323,3232,32211,32112,123123,321231,123123};
+	    Arrays.sort(x);
+	    String s = Arrays.toString(x);
+
+	    System.out.println(s);
+	}
+}
+		
