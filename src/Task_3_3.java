debaed4337cfeb2637f0617b4cb9ea5c721dc497
LuckyTiger3 <nikita2501400@gmail.com>
2018-09-04 15:09:52 +0300

    Task 3 3

---
+++ src/Task_3_3.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+public class Task_3_3 {
+
+	public static void main(String[] args) {
+		
+		int x[] = {1, 2, 3, 4};
+		String s = Arrays.toString(x);
+		System.out.println(s);
+		
+		int m1[] = {x[0], x[1]};
+		String z = Arrays.toString(m1);
+		System.out.println(z);
+		
+		int m2[] = {x[2], x[3]};
+		String c = Arrays.toString(m2);
+		System.out.println(c);
+		
+
+	}
+
+}
